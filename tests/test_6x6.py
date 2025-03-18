71896e00e6e103bb4a87f628e44c8e0c5acc29bd
Sal Mourad <sal.mourad95@gmail.com>
2025-03-18 14:06:38 -0700

    fixed manually determined max collected result that I forgot to change earlier

--- tests/test_6x6.py
+++ tests/test_6x6.py
@@ -91,8 +91,8 @@ def main():
     print("\nManually Determined Results:")
     print("\n-------------------------------------")
 
-    print("\nThe Best Path is: [(0, 0), (1, 0), (2, 0), (3, 0), (3, 1), (3, 2), (3, 3), (4, 3), (4, 4), (4, 5), (5, 5)]")
-    print("\nMaximum Value Collected: 43")
+    print("\nThe Best Path is: [(0, 0), (1, 0), (2, 0), (3, 0), (3, 1), (2, 1), (2, 2), (3, 2), (3, 3), (4, 3), (4, 4), (4, 5), (5, 5)]")
+    print("\nMaximum Value Collected: 57")
 
     print("\n-------------------------------------")
     print("\nAutogenrated Results Using Program:")
